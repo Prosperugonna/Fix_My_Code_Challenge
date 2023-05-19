33836817b1ea886a5773ce70f6409e8682ab5d5c
Prosperugonna <prosperugonna@gmail.com>
2023-05-19 03:22:40 -0700

    fix

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,32 +1,24 @@
-#!/usr/bin/python3
-""" FizzBuzz
-"""
-import sys
+#!/usr/bin/node
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
 
-def fizzbuzz(n):
-  """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
 
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
-    """
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print(i, end=" ")
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
+}
 
-    print()
+size = parseInt(process.argv[2], 10)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python 0-fizzbuzz.py <number>")
-        sys.exit(1)
-
-    n = int(sys.argv[1])
-    fizzbuzz(n)
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
+}
